9f947895aa8184e816ba5c74c16b7734b3a1a9d5
davi-cesar201 <dcovsnew@gmail.com>
2025-08-07 12:14:35 -0300

    .

---
+++ aula01/variaveis.py
@@ -0,0 +1,40 @@
+print("ola mundo!")
+
+nome = "Davi"
+print("nome , ",nome)
+idade = 17
+print("idade , ",idade)
+ano_nascimento = 2008
+print("ano_nascimento , ",ano_nascimento)
+altura = 1.91
+print("altura , ",altura)
+print ("Olá me chamo",nome,"tenho",idade,"nasci em",ano_nascimento,"e tenho",altura)
+
+ano = 2025
+print("ano atual",ano)
+ano += 1
+print("ano acrescido de +1",ano)
+ano -= 1
+print("ano decrecido de de -1",ano)
+
+#operadores lógicos
+#AND, OR, NOT
+#tipos de Varias
+#operadores
+
+print() #estrada de dados
+print(30*'-','entrada de daos',30*'-')
+nome_usuario = input("digite o seu nome: " )
+
+
+# passo inpu por pradrão ele vem com str
+print()
+print(30*'-','entrada de dados' ,30*'-')
+dados_usuario = input("coloque o seu email: ")
+senha_email = input("digite sua senha: ")
+print("seja bem vindo ao nosso site", nome_usuario)
+
+saudacao = input("digite seu nome: ")
+cpf = input("digite seu telefone: ")
+print(20*)
+
