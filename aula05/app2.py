b1b7f7962e73e7ddbe460603183b4e81465a0b2b
davi-cesar201 <dcovsnew@gmail.com>
2025-08-21 12:25:36 -0300

    mensagem commit

---
+++ aula05/app2.py
@@ -0,0 +1,8 @@
+#manipulação de arquivos
+#leitura de arquivos
+with open('texto.txt', 'r', encoding= 'utf-8') as f:
+    texto =f.read()
+
+print(texto)
+
+    
---
+++ aula05/variaveis.py
@@ -0,0 +1,22 @@
+while True:
+   try:
+      #entrada de dados
+      etanol= float(input('digite o preço do etanol: ').replace(',','.'))
+      gasolina=float(input('digite o preço da gasolina: ').replace(',','.'))
+      calculo=(etanol/gasolina)*100
+      resultado= "gasolina" if calculo > 70 else 'Etanol'
+
+      print(f'resultado= {calculo:.2f}%. compensa abastecer com {resultado}.')
+
+      opcao= input('deseja fazer o calculo? (s/n)').lower().strip()
+      match opcao:
+         case 's':
+            continue
+         case 'n':
+            break
+         case _:
+            print(f'opção inválida!')
+            continue
+   except Exception as e:
+    print(f'Não foi possível executar a operação {e}')
+    continue              
